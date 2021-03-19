9c4381ca63a62a41870c0b290d3f47dff8663eed
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2021-03-20 01:42:31 +0530

    wayyy harder than i thought

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,28 @@
-function editsite() {
-    console.log('this');
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-        let url = tabs[0].url;
+// function editsite() {
+//     // console.log('this');
+//     // chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+//     //     let url = tabs[0].url;
+//     // });
+//     // var s = document.createElement('script');
+//     // s.src = chrome.extension.getURL('script.js');
+//     // (document.head || document.documentElement).appendChild(s);
+//     // s.onload = function() {
+//     //     s.parentNode.removeChild(s);
+//     // };
+//     //     var actualCode = `// Code here.
+//     //     document.designMode = "on";
+//     // `;
+
+//     //     var script = document.createElement('script');
+//     //     script.textContent = actualCode;
+//     //     (document.head || document.documentElement).appendChild(script);
+//     //     script.remove();
+//     document.designMode = "on";
+// }
+
+$(document).ready(function() {
+    $("abcd").click(function() {
+        document.designMode = "on";
+        alert('hello');
     });
-    var s = document.createElement('script');
-    s.src = chrome.extension.getURL('script.js');
-    (document.head || document.documentElement).appendChild(s);
-    s.onload = function() {
-        s.parentNode.removeChild(s);
-    };
-}
+});
