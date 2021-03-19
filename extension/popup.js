6dda4558497d48219a221fe6e7a456e9f4509ef6
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2021-03-20 01:19:48 +0530

    did random shit

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,12 @@
 function editsite() {
     console.log('this');
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        let url = tabs[0].url;
+    });
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('script.js');
+    (document.head || document.documentElement).appendChild(s);
+    s.onload = function() {
+        s.parentNode.removeChild(s);
+    };
 }
