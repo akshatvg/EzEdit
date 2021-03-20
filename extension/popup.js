cfa6f25c393f079de5f8c375642a906a05c35f0a
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-03-20 19:01:32 +0530

    Added Push to github

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var style = document.createElement("link");
-    style.rel = "stylesheet";
-    style.type = "text/css";
-    style.href = chrome.extension.getURL("style.css");
-    (document.head || document.documentElement).appendChild(style);
-    document.getElementById("editBtnEzEdit").addEventListener("click", sendDetails);
+document.addEventListener("DOMContentLoaded", function () {
+  var style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.type = "text/css";
+  style.href = chrome.extension.getURL("style.css");
+  (document.head || document.documentElement).appendChild(style);
+  document
+    .getElementById("submitEditBtnEzEdit")
+    .addEventListener("click", sendDetails);
 
-<<<<<<< HEAD
   document.getElementById("signinEzEdit").addEventListener("click", signin);
 
   function signin() {
@@ -22,15 +23,3 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 });
-=======
-    document.getElementById("submitEditBtnEzEdit").addEventListener("click", sendDetails);
-
-    function sendDetails() {
-        console.log("Reached here: Send details");
-        document.getElementById("editBtnEzEdit").disabled = "true";
-        chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, "edit");
-        });
-    }
-});
->>>>>>> c6c0acfbd88139ec1140841918ecdfc399df34b1
