bba921efd3e4831e590359fd642ccdb53342fd71
Sai Sandeep Rayanuthala <raynuthalas@gmail.com>
2021-03-20 16:40:58 +0530

    Added index.html

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.type = 'text/css';
-    style.href = chrome.extension.getURL('style.css');
-    (document.head || document.documentElement).appendChild(style);
-    document.getElementById('editBtnEzEdit').addEventListener('click', sendDetails);
+document.addEventListener("DOMContentLoaded", function () {
+  var style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.type = "text/css";
+  style.href = chrome.extension.getURL("style.css");
+  (document.head || document.documentElement).appendChild(style);
+  document
+    .getElementById("editBtnEzEdit")
+    .addEventListener("click", sendDetails);
 
-    function sendDetails() {
-        console.log('Reached here: Send details');
-        document.getElementById("editBtnEzEdit").disabled = "true";
-        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, "edit");
-        })
-    }
+  function sendDetails() {
+    console.log("Reached here: Send details");
+    document.getElementById("editBtnEzEdit").disabled = "true";
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, "edit");
+    });
+  }
 });
