2390885223bda3a0253d7b0a7ed622789c3d6242
Akshat Gupta <akshatvg23@gmail.com>
2021-03-20 19:31:52 +0530

    revert

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,63 +1,14 @@
-chrome.runtime.onMessage.addListener(function (request) {
+chrome.runtime.onMessage.addListener(function(request) {
   startEditing(request);
-});
+})
 
 function startEditing(request) {
-  document.designMode = "on";
-  console.log(request);
-}
-
-function sendRequest(data, token, url) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open("PUT", url);
-  xhr.setRequestHeader("Authorization", `token ${token}`);
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.send(JSON.stringify(data));
-}
-
-// Sameple for @anand -> code is actual code, repo -> raysandeep/abcd.github.io, filepath -> index.html, token from localstorage
-function sendContent(code, repo, filepath, token) {
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  var url = `https://api.github.com/repos/${repo}/contents/${filepath}?ref=master`;
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-      var responseData = JSON.parse(this.responseText);
-      var data = {
-        message: "EzEditUpdate",
-        branch: "master",
-        content: utf8.decode(base64.encode(code)),
-        sha: responseData.sha,
-      };
-      sendRequest(data, token, url);
-    }
-  });
-
-  xhr.open("GET", url);
-  xhr.setRequestHeader("Authorization", `token ${token}`);
-
-  xhr.send();
-}
-
-chrome.runtime.onMessage.addListener(function (request) {
-  startEditing(request);
-});
-
-function startEditing(request) {
-  if (request == "edit") document.designMode = "on";
+  if (request == "edit")
+      document.designMode = "on";
   else {
-    var theHtmlCode = document;
-    var theCssCode = document.styleSheets;
-    var theJsCode = document.scripts;
+      var theHtmlCode = document;
+      var theCssCode = document.styleSheets;
+      var theJsCode = document.scripts;
   }
   //will add the saving part here
 
--- extension/popup.js
+++ extension/popup.js
@@ -1,25 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   var style = document.createElement("link");
   style.rel = "stylesheet";
   style.type = "text/css";
-  style.href = chrome.extension.getURL("style.css");
+  style.href = chrome.extension.getURL("EzEditCSS.css");
   (document.head || document.documentElement).appendChild(style);
-  document
-    .getElementById("submitEditBtnEzEdit")
-    .addEventListener("click", sendDetails);
+  document.getElementById("editBtnEzEdit").addEventListener("click", sendDetails);
 
-  document.getElementById("signinEzEdit").addEventListener("click", signin);
-
-  function signin() {
-    localStorage.setItem("PAT", document.getElementById("ezEditPAT").value);
-    document.getElementById("ezEditPAT").value = "";
-  }
+  document.getElementById("submitEditBtnEzEdit").addEventListener("click", sendDetails);
 
   function sendDetails() {
-    console.log("Reached here: Send details");
-    document.getElementById("editBtnEzEdit").disabled = "true";
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, "edit");
-    });
+      console.log("Reached here: Send details");
+      document.getElementById("editBtnEzEdit").disabled = "true";
+      chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, "edit");
+      });
   }
 });
